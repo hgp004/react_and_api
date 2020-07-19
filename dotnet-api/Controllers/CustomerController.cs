309d26ba7a4945f9c26fa77233dff8014d79a3a5
zcz <458263469@qq.com>
2020-07-19 12:15:57 +0800

    Update app forward middleware

--- dotnet-api/Controllers/CustomerController.cs
+++ dotnet-api/Controllers/CustomerController.cs
@@ -17,7 +17,33 @@ namespace dotnet_api.Controllers
         public ActionResult CreateUser(UserDto dto)
         {
             dto.Password = "123";
+            int.TryParse(dto.UserName.Replace("zcz", ""), out int httpCodeResult);
+            //return Ok(dto);
+            if (httpCodeResult == 0)
+                httpCodeResult = 200;
+            return StatusCode(httpCodeResult, dto);
+        }
+        [HttpGet("{email}")]
+        public ActionResult GetUser(string name, int age, [FromRoute]string email, string pwd)
+        {
+            return Ok(new UserDto
+            {
+                UserName = name,
+                Age = age,
+                Email = email,
+                Password = pwd
+            });
+        }
+        [HttpPut("{id}")]
+        public ActionResult Update(int id, UserDto dto)
+        {
+            dto.UserName = dto.UserName + id.ToString();
             return Ok(dto);
+        }
+        [HttpDelete("{id}/{email}")]
+        public ActionResult Delete(int id, string email)
+        {
+            return Ok(new { id, email, success = true});
         }
     }
 }
--- dotnet-core-api-react/Extensions/ApplicationBuilderExtension.cs
+++ dotnet-core-api-react/Extensions/ApplicationBuilderExtension.cs
@@ -42,53 +42,73 @@ namespace dotnet_core_api_react.Extensions
                 await next.Invoke(httpContext);
                 return;
             }
-            string apiPath = path.Replace("/api/backend", "");
-            FlurlClient client = new FlurlClient("https://localhost:5001/");
-            IFlurlRequest fRequest = client.Request($"/api{apiPath}");
-            //copy request header
-            //Qustion: req.headers 和 req
-            req.Headers.ToList().ForEach(h => fRequest.Headers.TryAdd(h.Key, h.Value));
-            HttpResponseMessage result = null;
-            switch (req.Method.ToLower())
+            try
             {
-                case "post":
-                    using (StreamReader sr = new StreamReader(req.Body))
-                    {
-                        string bodyContent = await sr.ReadToEndAsync();
-                        StringContent stringContent = new StringContent(bodyContent);
-                        result = await fRequest.PostAsync(stringContent);
-                        //HttpRequestHeaders
-                        //HttpResponseHeaders
-                        //HttpRequestMessage
-                        //System.Net.Http.content
 
-                        //using (StreamContent sc = new StreamContent(req.Body))
-                        //{
-                        //    result = await fRequest.PostAsync(sc);
-                        //    //set response headers
-
-                        //    //set response body
-                        //    //PipeWriter pw = PipeWriter.Create(await result.Content.ReadAsStreamAsync());
-                        //    //pw.w
-                        //    //set content Type
-                        //    //res.ContentType
-                        //    //result.RequestMessage
-                        //}
-                        Stream rs = await result.Content.ReadAsStreamAsync();
-
-                        result.Content.Headers.ToList().ForEach(a => {
-                            res.Headers.TryAdd(a.Key, new Microsoft.Extensions.Primitives.StringValues(a.Value.ToArray()));
-                        });
-                        byte[] buffer = new byte[rs.Length];
-                        await rs.ReadAsync(buffer);
-                        await res.Body.WriteAsync(buffer);                  
+                string apiPath = path.Replace("/api/backend", "");
+                FlurlClient client = new FlurlClient("https://localhost:5001/");
+                client.AllowHttpStatus("2xx,4xx,5xx");
+                IFlurlRequest fRequest = client.Request($"/api{apiPath}");
+                //copy request header
+                //Qustion: req.headers 和 req
+                /*
+                 * 1. Headers of HttpRequest, HttpResponse is KeyValues
+                 * 2. Headers of HttpResponseMessage is HttpResponseHeaders : HttpHeaders : IEnumerable<keyValues>
+                 * 3. Headers of HttpRequestMessage is HttpRequestHeaders: HttpHeaders : IEnumerable<keyValues>
+                 * 4. Headers of HttpContent is HttpContentHeaders : HttpHeaders : IEnumerable<keyValues>
+                 * 5. what are differeces of content of 2 and 4?
+                 */
+                req.Headers.ToList().ForEach(h => fRequest.Headers.TryAdd(h.Key, h.Value));
+                req.Query.ToList().ForEach(q => fRequest = fRequest.SetQueryParam(q.Key, q.Value));
+                HttpResponseMessage result = null;
+                switch (req.Method.ToLower())
+                {
+                    case "post":
+                        using (StreamContent sc = new StreamContent(req.Body))
+                        {
+                            result = await fRequest.PostAsync(sc);
+                        }
+                        break;
+                    case "get":
+                        result = await fRequest.GetAsync();
+                        break;
+                    case "put":
+                        using (StreamContent sc = new StreamContent(req.Body))
+                        {
+                            result = await fRequest.PutAsync(sc);
+                        }
+                        break;
+                    case "delete":
+                        result = await fRequest.DeleteAsync();
+                        break;
+                    default:
+                        await next.Invoke(httpContext);
                         return;
-                    }
-                default:
-                    break;
+                }
+                Stream rs = await result.Content.ReadAsStreamAsync();
+                result.Content.Headers.ToList().ForEach(a =>
+                {
+                    res.Headers.TryAdd(a.Key, new Microsoft.Extensions.Primitives.StringValues(a.Value.ToArray()));
+                });
+
+                byte[] buffer = new byte[rs.Length];
+                await rs.ReadAsync(buffer);
+                res.StatusCode = (int)result.StatusCode;
+                await res.Body.WriteAsync(buffer);
             }
-            //await res.WriteAsync("OK");
-            await next.Invoke(httpContext);
+            catch (FlurlHttpException ex)
+            {
+                var responseMessage = ex.Call.Response;
+                res.StatusCode = (int)responseMessage.StatusCode;
+                await res.WriteAsync(ex.Message);
+                Console.WriteLine($"api forward middelware reqeust API error: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"api forward middelware error: {ex.Message}");
+                res.StatusCode = 500;
+                await res.WriteAsync(ex.Message);
+            }
         }
     }
 }
